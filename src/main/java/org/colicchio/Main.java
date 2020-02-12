03e50279d4541d399701c9f319e69a1a65a12938
Joshua Colicchio <joshuacolicchio@gmail.com>
2020-02-12 17:44:48 -0500

    Update Main.java
    Added code for iteration 2.
    Reorganized code to be separated into iteration blocks.

--- src/main/java/org/colicchio/Main.java
+++ src/main/java/org/colicchio/Main.java
@@ -1,15 +1,19 @@
 package org.colicchio;
 
 import com.opencsv.exceptions.CsvValidationException;
-import org.jsoup.nodes.Element;
 import java.io.IOException;
+import org.jsoup.nodes.Element;
 
 public class Main {
   public static void main(String[] args) throws IOException, CsvValidationException {
-    // CSV parsing
+
+    /* Iteration 1: Data Flow Operations
+     * Create a Maven project with OpenCSV working & read & print the contents
+     * of a file (an action called parsing). */
+
+    // CSV Parsing
     CsvParser csvParser = new CsvParser("src/main/resources/bookstore_report2.csv");
     csvParser.printCsv();
-
     System.out.println("\n\n"); // 3 lines between parsing groups
 
     // JSON parsing
@@ -18,12 +22,24 @@ public class Main {
       System.out.println(
           author.getAuthorName() + " " + author.getAuthorEmail() + " " + author.getAuthorUrl());
     }
-
     System.out.println("\n\n"); // 3 lines between parsing groups
 
     // XML parsing
     for (Element e : XmlParser.readXml("src/main/resources/authors.xml").select("author")) {
       System.out.println(e.text());
     }
+    // End of Iteration 1
+
+    /* Iteration 2: Data Flow Operations
+     * Add JDBC & GSON to your Maven project, read the contents of a CSV,
+     * and insert this into the provided database.
+     * You will need to insert into "author" from the json file (the three fields in the json file)
+     * And "book" with some of the information from the csv file (see bottom) */
+    csvParser.saveToDatabase();
+    for (Author author : authors) {
+      DatabaseManager.saveAuthor(author);
+    }
+    // Can't really do anything with the XML for iteration 2.
+    // End of Iteration 2
   }
 }
